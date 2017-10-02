9e0dbacba4b18d7256ee25463a90d31ae2b2bf78
Mike <mike@onlinesup.com>
2017-10-02 22:48:59 +0300

    Update init.lua

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -13,3 +13,23 @@ include("libs/role.lua")
 
 core.team.init(core.config.teams)
 core.role.init(core.config.roles)
+
+function GM:PlayerInitialSpawn( ply )
+ 
+    RunConsoleCommand( "team_menu" )
+ 
+end
+ 
+function GM:PlayerLoadout(ply) 
+ 
+	ply:StripWeapons()
+ 
+	if ply:Team() == 1 then
+		ply:Give("weapon_physcannon")
+ 
+	elseif ply:Team() == 2 then
+		ply:Give("weapon_physgun")
+ 
+	end
+ 
+end
