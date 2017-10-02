f40f36e748353f5eb039fba15649c1de01fc0a0c
Mike <mike@onlinesup.com>
2017-10-02 22:35:49 +0300

    Update team.lua

--- gamemode/libs/team.lua
+++ gamemode/libs/team.lua
@@ -2,14 +2,13 @@ core.team = {}
 core.team.store = {}
 core.team.uniqIndex = 0
 
-function core.team.addTeam(name, desc, access)
+function core.team.addTeam(name, desc)
   core.team.uniqIndex = core.team.uniqIndex + 1
 
   core.team.store[core.team.uniqIndex] = {
     index = core.team.uniqIndex,
     name = name or "Unknown Team",
     description = desc or "N/A",
-    access = access or "",
     groups = {},
     roles = {},
     gangs = {}
@@ -18,3 +17,8 @@ function core.team.addTeam(name, desc, access)
   return core.team.uniqIndex
 end
 
+function core.team.init(teams)
+  for teamKey, team in pairs(teams) do
+    core.team.addTeam(team.title, team.desc)
+  end
+end
