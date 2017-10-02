72f57c3b16894342666d9062d56a426a7d03ee88
Mike <mike@onlinesup.com>
2017-10-02 21:18:28 +0300

    Update group.lua

--- gamemode/libs/group.lua
+++ gamemode/libs/group.lua
@@ -6,7 +6,7 @@ function core.group.addGroup(title, desc, access)
   core.group.uniqIndex = core.group.uniqIndex + 1
   
   core.group.store[core.group.uniqIndex] = {
-    index = core.team.uniqIndex,
+    index = core.group.uniqIndex,
 		name = name or "Unknown Group",
 		description = desc or "N/A",
 		access = access or "",
