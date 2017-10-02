c9855788acaa632d6280b771e58c2dec1307e2db
Mike <mike@onlinesup.com>
2017-10-02 21:52:27 +0300

    Update init.lua

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -2,5 +2,10 @@ core = {}
 
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
+AddCSLuaFile("libs/team.lua")
+AddCSLuaFile("libs/group.lua")
+AddCSLuaFile("libs/role.lua")
+
+core.role.init(core.config.roles)
 
 include( "shared.lua" )
