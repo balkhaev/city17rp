88266b56ca4958bc8a71fe8018e23eac528cb72d
Mike <mike@onlinesup.com>
2017-10-02 20:44:46 +0300

    Update shared.lua

--- gamemode/config/shared.lua
+++ gamemode/config/shared.lua
@@ -5,8 +5,63 @@ core.config.defaults = {
 	spawnTime = 20
 }
 
+core.config.teams = {
+  civilians = {
+    title = "Мирные жители"
+  },
+  rebels = {
+    title = "Повстанцы"
+  },
+  alliance = {
+    title = "Альянс"
+  },
+  zombie = {
+    title = "Зомби"
+  }
+}
+
+core.config.groups = {
+  administration = {
+    title = "Администрация города",
+    team = "civilians"
+  },
+  traders = {
+    title = "Торговцы",
+    team = "civilians"
+  },
+  citizens = {
+    title = "Жители",
+    team = "civilians"
+  },
+  civilianDefence = {
+    title = "Гражданская оборона",
+    team = "alliance"
+  },
+  allianceArmy = {
+    title = "Армия Альянса",
+    team = "alliance"
+  },
+  implementationDepartment = {
+    title = "Отдел внедрения",
+    team = "rebels"
+  },
+  rebelsArmy = {
+    title = "Армия Повстанцев",
+    team = "rebels"
+  }
+}
+
 core.config.roles = {
 	citizen = {
-
-	}
+    title = "Мирный житель",
+    group = "citizens"
+	},
+  rebelsCitizen = {
+    title = "Доверенное лицо Повстанцев",
+    group = "citizens"
+  },
+  allianceCitizen = {
+    title = "Доверенное лицо Альянса",
+    group = "citizens"
+  }
 }
