5e4d9510ed12d7283e97460cce529cd99f92a489
Mike <mike@onlinesup.com>
2017-10-02 23:26:07 +0300

    Update group.lua

--- gamemode/libs/group.lua
+++ gamemode/libs/group.lua
@@ -2,15 +2,37 @@ core.group = {}
 core.group.store = {}
 core.group.uniqIndex = 0
 
-function core.group.addGroup(title, desc, access)
-  core.group.uniqIndex = core.group.uniqIndex + 1
+function core.group.addGroup(name, desc, teamName, access)
+  local team = core.team.getTeam(teamName)
   
-  core.group.store[core.group.uniqIndex] = {
-    index = core.group.uniqIndex,
-		name = name or "Unknown Group",
-		description = desc or "N/A",
-		access = access or "",
-		roles = {},
-		gangs = {}
-  }
+  if team then
+    core.group.uniqIndex = core.group.uniqIndex + 1
+
+    core.group.store[core.group.uniqIndex] = {
+      index = core.group.uniqIndex,
+      name = name or "Unknown Group",
+      description = desc or "N/A",
+      access = access or "",
+      roles = {},
+      gangs = {}
+    }
+    
+    core.team.addGroup(core.group.uniqIndex, name, team.index)
+    
+    return core.group.uniqIndex
+  end
+end
+
+function core.group.addGang(playerId, playerName, groupIndex)
+  core.group.store[groupIndex].gangs[playerId] = playerName
+end
+
+function core.group.addRole(roleIndex, roleName, groupIndex)
+  core.group.store[groupIndex].roles[roleIndex] = roleName
+end
+
+function core.group.init(groups)
+  for groupKey, group in pairs(groups) do
+    core.group.addGroup(group.name, group.desc, group.team, group.access)
+  end
 end
