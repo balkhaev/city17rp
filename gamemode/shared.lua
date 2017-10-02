6f3b341689465824cf4c836002fbc46c0633b0a5
Mike <mike@onlinesup.com>
2017-10-02 22:37:44 +0300

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,3 +2,7 @@ GM.Name = "City 17 RP"
 GM.Author = "Narkeba"
 GM.Email = "narkeba@gmail.com"
 GM.Website = "narkeba.name"
+
+for teamKey, team in pairs(core.team.store) do
+  team.SetUp(team.index, team.name, team.color)
+end
