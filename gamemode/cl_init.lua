e0b4c780cc4b056b3454790c55a0ca3801a318f0
Mike <mike@onlinesup.com>
2017-10-03 23:25:09 +0300

    Update cl_init.lua

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -6,9 +6,12 @@ include("libs/core.lua")
 include("libs/role/team.lua")
 include("libs/role/group.lua")
 include("libs/role/role.lua")
+include("libs/panel.lua")
 
 core.init(core.config)
 
 for teamKey, teamVal in pairs(core.team.store) do
   team.SetUp(teamVal.index, teamVal.name, teamVal.color)
 end
+
+core.panel.createPanel()
