8f282c342667a195968248dc398c708d99441257
Mike <mike@onlinesup.com>
2017-10-03 21:18:00 +0300

    Update core.lua

--- gamemode/libs/core.lua
+++ gamemode/libs/core.lua
@@ -1,4 +1,7 @@
 function core.init(config)
+  Msg("=======Init core=======\n")
+  PrintTable(core.config)
+
   core.team.init(config.teams)
   core.group.init(config.groups)
   core.role.init(config.roles)
