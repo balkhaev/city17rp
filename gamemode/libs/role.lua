a53b290b476a46bcd234fe4aef1f81006a52380b
Mike <mike@onlinesup.com>
2017-10-03 22:57:31 +0300

    Update role.lua

--- gamemode/libs/role.lua
+++ gamemode/libs/role.lua
@@ -67,7 +67,6 @@ end
 
 function core.role.getPlayerRole(ply)
   local roleIndex = core.role.indexPlayerRole(ply)
-  Msg("Index Role ", roleIndex, " of player ", ply:Nick())
 
   if roleIndex == -1 then
     return nil
