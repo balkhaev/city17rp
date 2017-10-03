63d854b05afd98a6984780f573c76dc669782453
Mike <mike@onlinesup.com>
2017-10-03 09:22:07 +0300

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,15 +2,3 @@ GM.Name = "City 17 RP"
 GM.Author = "Narkeba"
 GM.Email = "narkeba@gmail.com"
 GM.Website = "narkeba.name"
-
-include("config/general.lua")
-include("libs/team.lua")
-include("libs/group.lua")
-include("libs/role.lua")
-
-core.team.init(core.config.teams)
-core.role.init(core.config.roles)
-
-for teamKey, team in pairs(core.team.store) do
-  team.SetUp(team.index, team.name, team.color)
-end
