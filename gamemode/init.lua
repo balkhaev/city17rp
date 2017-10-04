b2de629b60d196602f4b15871a69b29ef91e56dd
Mike <mike@onlinesup.com>
2017-10-04 21:25:56 +0300

    Update init.lua

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -45,7 +45,7 @@ concommand.Add("getspawnpoint", function(ply, cmd, args)
   local x,y,z = ply:GetPos().x,ply:GetPos().y,ply:GetPos().z
   Msg(x..","..y..","..z)
 end)
---[[
+
 concommand.Add("setrole", function(ply, cmd, args)
   if args[1] == nil then
     return
@@ -57,4 +57,3 @@ concommand.Add("setrole", function(ply, cmd, args)
 
   core.role.setPlayerRole(ply, args[1])
 end)
---]]
