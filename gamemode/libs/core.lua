fac86970c77435b2c664c5c214082de7cf70cdf5
Mike <mike@onlinesup.com>
2017-10-04 19:49:27 +0300

    Update core.lua

--- gamemode/libs/core.lua
+++ gamemode/libs/core.lua
@@ -3,13 +3,13 @@ function core.init(config)
   PrintTable(config.defaults)
 
   Msg("--- Init teams ---\n")
-  for teamKey, team in pairs(teams) do
+  for teamKey, team in pairs(core.config.teams) do
     Msg("Adding team "..team.title.."\n")
     core.team.addTeam(teamKey, team)
   end
 
   Msg("--- Init groups ---\n")
-  for groupKey, group in pairs(groups) do
+  for groupKey, group in pairs(core.config.groups) do
     Msg("Adding role "..group.title.."\n")
     core.group.addGroup(groupKey, group)
   end
