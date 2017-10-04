4cc19f6fdaa02368399d9218ea419b407162e721
Mike <mike@onlinesup.com>
2017-10-04 22:55:52 +0300

    Update cl_panel.lua

--- gamemode/cl_panel.lua
+++ gamemode/cl_panel.lua
@@ -1,25 +1,12 @@
+local menuIsOpen  = false
+
 hook.Add("Think", "openGeneralMenu", function()
-  if input.WasKeyPressed(27) then
-    core.panel.createPanel(ply)
-  end
-  if input.WasKeyReleased(27) then
+
+  if input.IsKeyDown(KEY_Q) and menuIsOpen == false then
+		menuIsOpen = true -- Enables antispam, doesn't make them like say it 5000 times a second lol
+		core.panel.createPanel(LocalPlayer())
+  elseif menuIsOpen == true then
     core.panel.destroyPanel()
   end
+
 end)
-
---[[
-local antispam 	= false -- Dont edit c:
-local time 		= 60 -- Set your anti spam time here
-
-hook.Add("Think", "Call_Admin_Thingy", function()
-  Msg("Think")
-  if input.IsKeyDown(27) and antispam == false then
-		antispam = true -- Enables antispam, doesn't make them like say it 5000 times a second lol
-		core.panel.createPanel(ply)
-
-		timer.Simple(time, function() -- This part resets the antispam, making it re-useable after _N_ seconds.
-			antispam = false
-		end)
-  end
-end)
---]]
