84301708c1b4906dbeb2e763a7c70b28e466f179
Mike <mike@onlinesup.com>
2017-10-04 23:44:17 +0300

    Update cl_panel.lua

--- gamemode/cl_panel.lua
+++ gamemode/cl_panel.lua
@@ -5,7 +5,7 @@ hook.Add("Think", "openGeneralMenu", function()
   if input.IsKeyDown(KEY_Q) and menuIsOpen == false then
     menuIsOpen = true
     core.panel.createPanel(LocalPlayer())
-  elseif menuIsOpen == true then
+  elseif not input.IsKeyDown(KEY_Q) and menuIsOpen == true then
     core.panel.destroyPanel()
   end
 
