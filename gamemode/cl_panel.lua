1d3b4f8b92140ca366657a6c87b2a9791e7129b6
Mike <mike@onlinesup.com>
2017-10-04 21:45:27 +0300

    Update cl_panel.lua

--- gamemode/cl_panel.lua
+++ gamemode/cl_panel.lua
@@ -1,3 +1,16 @@
 hook.Add("PlayerKeyPress","BindMenu",function(ply,key)
+  Msg("PlayerKeyPress")
   if key == KEY_Q then core.panel.createPanel(ply) end
+end)
+
+hook.Add("Think", "Call_Admin_Thingy", function()
+  Msg("Think")
+	if input.IsKeyDown(KP_F9) and antispam == false then
+		antispam = true -- Enables antispam, doesn't make them like say it 5000 times a second lol
+		LocalPlayer():ConCommand("say /calladmin") -- makes them say the command
+
+		timer.Simple(time, function() -- This part resets the antispam, making it re-useable after _N_ seconds.
+			antispam = false
+		end)
+	end
 end)
