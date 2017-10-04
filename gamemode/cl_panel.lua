960aeb75c401bfd89f53ecd68691d22f8e1bfe52
Mike <mike@onlinesup.com>
2017-10-04 21:58:37 +0300

    Update cl_panel.lua

--- gamemode/cl_panel.lua
+++ gamemode/cl_panel.lua
@@ -1,11 +1,22 @@
-hook.Add("PlayerKeyPress","BindMenu",function(ply,key)
-  Msg("PlayerKeyPress")
-  if key == KEY_Q then core.panel.createPanel(ply) end
-end)
+GM:KeyPress( pl, key )
+	if key == "q" then
+		core.panel.createPanel()
+	end
+end
+
+--[[
+local antispam 	= false -- Dont edit c:
+local time 		= 60 -- Set your anti spam time here
 
 hook.Add("Think", "Call_Admin_Thingy", function()
   Msg("Think")
-  if input.IsKeyDown(27) then
-    core.panel.createPanel(ply)
+  if input.IsKeyDown(27) and antispam == false then
+		antispam = true -- Enables antispam, doesn't make them like say it 5000 times a second lol
+		core.panel.createPanel(ply)
+
+		timer.Simple(time, function() -- This part resets the antispam, making it re-useable after _N_ seconds.
+			antispam = false
+		end)
   end
 end)
+--]]
