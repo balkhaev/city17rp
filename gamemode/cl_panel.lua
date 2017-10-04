e86f8d61eb48a22596a60a09f265f4b448da817b
Mike <mike@onlinesup.com>
2017-10-04 23:02:17 +0300

    Update cl_panel.lua

--- gamemode/cl_panel.lua
+++ gamemode/cl_panel.lua
@@ -4,7 +4,7 @@ hook.Add("Think", "openGeneralMenu", function()
 
   if input.IsKeyDown(KEY_Q) and menuIsOpen == false then
     menuIsOpen = true -- Enables antispam, doesn't make them like say it 5000 times a second lol
-    core.panel.createPanel(LocalPlayer())
+    core.panel.createPanel(core.player)
   elseif menuIsOpen == true then
     core.panel.destroyPanel()
   end
