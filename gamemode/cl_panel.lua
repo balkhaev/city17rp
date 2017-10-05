39aaecf0120a971c0ad785df23d3400d147aa1b1
Mike <mike@onlinesup.com>
2017-10-05 20:31:43 +0300

    Update cl_panel.lua

--- gamemode/cl_panel.lua
+++ gamemode/cl_panel.lua
@@ -29,7 +29,7 @@ end )
 --]]
 
 hook.Add("PlayerKeyPress","BindMenu",function(ply,key)
-  Msg(ply:hasAccess("managment").."\n")
+  Msg(ply:hasAccess("managment"))
   if key == KEY_Q then
     if core.panel.isOpen then
       core.panel.destroyPanel()
