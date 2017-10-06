afe9367f93100da16767c5f0215143deb73fa3f6
balkhaev <mike@onlinesup.com>
2017-10-06 14:32:17 +0300

    Placeholder for zombie events

--- gamemode/libs/zombie.lua
+++ gamemode/libs/zombie.lua
@@ -1,6 +1,12 @@
 core.zombie = {}
 core.zombie.spawnPos = { Vector(87.167327880859,5650.2534179688,-463.96875), Vector(92.311859130859,7093.9091796875,-465.96875) }
+core.zombie.count = 0
 core.zombie.timer = 0
+core.zombie.isEvent = false
+
+GM:OnNPCKilled( function(npc, attacker, inflictor )
+
+end)
 
 function core.zombie.spawnZombie()
   local spawnpos = table.Random( core.zombie.spawnPos )
@@ -16,10 +22,35 @@ function core.zombie.spawnZombie()
   zombie:SetEnemy(ply)
   zombie:UpdateEnemyMemory( ply, plypos )
   zombie:SetSchedule( SCHED_CHASE_ENEMY  )
+
+  core.zombie.count = core.zombie.count + 1
 end
 
 function core.zombie.needSpawn()
+  --[[
+  if core.zombie.isEvent then
+
+  end
+  ]]--
   return core.zombie.timer < CurTime() and core.zombie.count <= core.config.defaults.zombieLimit
+end
+
+function core.zombie.isLimit()
+  if core.zombie.isEvent then
+    return true
+  end
+
+  local isZombieLimit = core.zombie.count <= core.config.defaults.zombieLimit
+
+  if isZombieLimit then
+    core.zombie.startEvent()
+  end
+
+  return isZombieLimit
+end
+
+function core.zombie.startEvent()
+  core.zombie.isEvent = true
 end
 
 function core.zombie.getNearestPlayer(zombie)
