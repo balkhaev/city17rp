10c90b5bc89f21f38894a83e0e02078a43104a3f
balkhaev <mike@onlinesup.com>
2017-10-13 18:05:09 +0300

    Relations for Alyx

--- gamemode/server/sv_npc.lua
+++ gamemode/server/sv_npc.lua
@@ -8,4 +8,15 @@ hook.Add( "InitPostEntity", "SpawnAlyx", function()
   alyx:SetPos( conf.alyxSpawnVector )
   alyx:SetName("Alyx")
   alyx:Spawn()
+
+  local allianceGangs = core.role.getTeamGangs("alliance")
+  local rebelsGangs = core.role.getTeamGangs("rebels")
+
+  for steamID, playerName in pairs(allianceGangs) do
+    alyx:AddEntityRelationship( steamID, D_FR, 99 )
+  end
+
+  for steamID, playerName in pairs(rebelsGangs) do
+    alyx:AddEntityRelationship( steamID, D_LI, 99 )
+  end
 end )
