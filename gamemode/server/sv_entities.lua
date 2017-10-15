808a3a94e86437abbd6ae9ebc56b31ded94027d6
balkhaev <mike@onlinesup.com>
2017-10-15 20:08:18 +0300

    Comment ammo create

--- gamemode/server/sv_entities.lua
+++ gamemode/server/sv_entities.lua
@@ -1,6 +1,8 @@
 hook.Add( "InitPostEntity", "SpawnAmmoCrate", function()
+  --[[
   local ammoCrate = ents.Create("cw_ammo_crate_regular")
   if not IsValid( ammoCrate ) then return end
   ammoCrate:SetPos( Vector( 3408.012207,-1483.628174,162.946945 ) )
   ammoCrate:Spawn()
+  ]]--
 end )
