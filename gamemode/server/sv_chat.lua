67aa86ca5dcf6994691aedb8f9fd1d867d818b04
balkhaev <mike@onlinesup.com>
2017-10-16 21:10:00 +0300

    Fix chat bugs

--- gamemode/server/sv_chat.lua
+++ gamemode/server/sv_chat.lua
@@ -1,26 +1,36 @@
-hook.Add("PlayerCanSeePlayersChat" , "ChatRadius" , function( text, teamOnly, p1 , p2 )
+function GM:PlayerCanSeePlayersChat(text, teamOnly, p1 , p2)
   if ( not IsValid( p1 ) or not IsValid( p2 ) ) then return false end
 
-  if string.lower(string.sub( text, 1, 4) ) == "/ooc" or string.sub( text, 1, 3) == "// " then
-    print("ooc worked")
+  if string.lower(string.sub( text, 1, 4) ) == "/ooc" or string.sub( text, 1, 2) == "//" then
     return true
   end
 
   if teamOnly then
-    print("team only worked")
     return p1:Team() == p2:Team()
   end
 
-  return (p1:GetPos():Distance(p2:GetPos()) <= core.config.defaults.chatRadius)
-end)
+  if p1:GetPos():Distance(p2:GetPos()) <= core.config.defaults.chatRadius then
+    if string.sub(text, 1, 3) == "/me" then
+      p1:ChatPrint(p2:Nick().." "..string.sub(text, 4))
+      return false
+    end
+
+    if string.lower(text) == "/roll" then
+      p1:ChatPrint(p2:Nick().." выкинул "..math.random(1,100)..".")
+      return false
+    end
+
+    return true
+  end
+
+  return false
+end
 
 hook.Add( "PlayerSay", "Chat", function (pl, text, teamonly )
-  if string.lower(string.sub(text, 1, 4)) == "/ooc" then
+  if string.sub( text, 1, 2) == "//" then
+    return "[OOC] ".. string.sub( text, 3 )
+  end
+  if string.lower(string.sub( text, 1, 4) ) == "/ooc" then
     return "[OOC]".. string.sub( text, 5 )
-  elseif string.lower(text) == "/roll" then
-    return pl:Nick().." выкинул "..math.random(1,100).."."
-  elseif string.sub(text, 1, 3) == "/me" then
-    chat.AddText( Color( 100, 100, 255 ), pl, pl:Nick().." "..string.sub(text, 5) )
-    return pl:Nick().." "..string.sub(text, 5)
   end
 end)
