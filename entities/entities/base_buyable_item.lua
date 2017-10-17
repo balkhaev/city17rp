4e12534158b893f4ce90ba5729a24ce6b23f6bf2
balkhaev <mike@onlinesup.com>
2017-10-17 17:18:43 +0300

    Base buyable items and idcard version 2

---
+++ entities/entities/base_buyable_item.lua
@@ -0,0 +1,120 @@
+AddCSLuaFile()
+
+DEFINE_BASECLASS( "base_anim" )
+
+ENT.PrintName = "Buyable Item"
+ENT.Author = "Narkeba"
+ENT.Information = "Base Buyable Item"
+ENT.Category = "Buyable Items"
+
+ENT.Editable = true
+ENT.Spawnable = true
+ENT.AdminOnly = false
+ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
+
+ENT.MinCost = 1
+ENT.MaxCost = 10000
+
+function ENT:SetupDataTables()
+
+  self:NetworkVar( "Float", 0, "Cost", { KeyName = "costprice", Edit = { type = "Float", min = self.MinCost, max = self.MaxCost, order = 1 } } )
+
+end
+
+function ENT:SpawnFunction( ply, tr, ClassName )
+
+  if not tr.Hit then return end
+
+  local size = math.random( 16, 48 )
+
+  local ent = ents.Create( ClassName )
+  ent:SetPos( tr.HitPos + tr.HitNormal * size )
+  ent:Spawn()
+  ent:Activate()
+
+  return ent
+
+end
+
+function ENT:Initialize()
+
+  -- We do NOT want to execute anything below in this FUNCTION on CLIENT
+  if ( CLIENT ) then return end
+
+  -- Use the helibomb model just for the shadow (because it's about the same size)
+  self:SetModel( "models/Combine_Helicopter/helicopter_bomb01.mdl" )
+
+  -- We will put this here just in case, even though it should be called from OnBallSizeChanged in any case
+  self:RebuildPhysics()
+
+  -- Select a random color for the ball
+  self:SetBallColor( table.Random( {
+    Vector( 1, 0.3, 0.3 ),
+    Vector( 0.3, 1, 0.3 ),
+    Vector( 1, 1, 0.3 ),
+    Vector( 0.2, 0.3, 1 ),
+  } ) )
+
+end
+
+function ENT:RebuildPhysics( value )
+
+  -- This is necessary so that the vphysics.dll will not crash when attaching constraints to the new PhysObj after old one was destroyed
+  -- TODO: Somehow figure out why it happens and/or move this code/fix to the constraint library
+  self.ConstraintSystem = nil
+
+  local size = math.Clamp( value or self:GetBallSize(), self.MinSize, self.MaxSize ) / 2.1
+  self:PhysicsInitSphere( size, "metal_bouncy" )
+  self:SetCollisionBounds( Vector( -size, -size, -size ), Vector( size, size, size ) )
+
+  self:PhysWake()
+
+end
+
+function ENT:OnBallSizeChanged( varname, oldvalue, newvalue )
+
+  -- Do not rebuild if the size wasn't changed
+  if ( oldvalue == newvalue ) then return end
+
+  self:RebuildPhysics( newvalue )
+
+end
+
+function ENT:OnTakeDamage( dmginfo )
+
+  -- React physically when shot/getting blown
+  self:TakePhysicsDamage( dmginfo )
+
+end
+
+function ENT:Use( activator, caller )
+
+  self:Remove()
+
+  if ( activator:IsPlayer() ) then
+
+    -- Give the collecting player some free health
+    local health = activator:Health()
+    activator:SetHealth( health + 5 )
+    activator:SendLua( "achievements.EatBall()" )
+
+  end
+
+end
+
+if ( SERVER ) then return end -- We do NOT want to execute anything below in this FILE on SERVER
+
+function ENT:Draw()
+
+  local pos = self:GetPos()
+  local lcolor = render.ComputeLighting( pos, Vector( 0, 0, 1 ) )
+  local c = self:GetBallColor()
+
+  lcolor.x = c.r * ( math.Clamp( lcolor.x, 0, 1 ) + 0.5 ) * 255
+  lcolor.y = c.g * ( math.Clamp( lcolor.y, 0, 1 ) + 0.5 ) * 255
+  lcolor.z = c.b * ( math.Clamp( lcolor.z, 0, 1 ) + 0.5 ) * 255
+
+  local size = math.Clamp( self:GetBallSize(), self.MinSize, self.MaxSize )
+  render.DrawSprite( pos, size, size, Color( lcolor.x, lcolor.y, lcolor.z, 255 ) )
+
+end
